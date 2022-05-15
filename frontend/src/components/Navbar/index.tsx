e1562d0e39bcdaabf2340081725a63ada6ea6765
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2022-05-15 11:12:47 -0300

    changed github contact name

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -14,7 +14,7 @@ function Navbar() {
           >
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/rochaeduardo</p>
             </div>
           </a>
         </div>
